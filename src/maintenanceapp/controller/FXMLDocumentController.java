0fa402886f77b9a4a18fa7dd0bd8974e8989d7d9
owwix <53359125+owwix@users.noreply.github.com>
2022-12-22 16:56:30 -0800

    mvc file structure

--- src/maintenanceapp/controller/FXMLDocumentController.java
+++ src/maintenanceapp/controller/FXMLDocumentController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
  */
-package maintenanceapp;
+package maintenanceapp.controller;
 
 import maintenanceapp.model.Datasource;
 
